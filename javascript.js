f1822c3244d7215bc766a8f4470b10a9e90a7ea0
Felpz <fp19430@gmail.com>
2024-05-15 16:45:56 -0300

    atualizando de novo

--- javascript.js
+++ javascript.js
@@ -1,57 +1,44 @@
-var valor1
-var valor2
-if (valor2 == undefined){
-    valor2 = ""
-}
+const teclado = [document.getElementById('tecla_1'),
+document.getElementById('tecla_2'), document.getElementById('tecla_3')
+]
+var valor1 = []
+var num1
+var num2
+var result
 
 function deleteValor(array){
     let a = array.pop(array[array.length])
     document.getElementById('output_bar').innerText = array.join("")
-    return array
 }
 
-function exibeNumeros(ident){
-    teclado.forEach(macaco => 
-        macaco.addEventListener('click', function(){
-            event.preventDefault()
-            valor.push(teclado.indexOf(macaco) + 1)
-            document.getElementById(ident).innerText = valor.join("")
-            console.log(document.getElementById(ident).innerText)
-            document.getElementById('tecla_limpar').addEventListener('click', function(){
-                deleteValor(valor)
-            })
-        }))
-}
-
-
-const teclado = [document.getElementById('tecla_1'),
-document.getElementById('tecla_2'), document.getElementById('tecla_3')
-]
-
-var valor = [] 
-
-exibeNumeros('output_bar')
+teclado.forEach(macaco => 
+    macaco.addEventListener('click', function(){
+        event.preventDefault()
+        valor1.push(teclado.indexOf(macaco) + 1)
+        document.getElementById('output_bar').innerText = valor1.join("")
+        console.log(document.getElementById('output_bar').innerText)
+        document.getElementById('tecla_limpar').addEventListener('click', function(){
+        deleteValor(valor1)
+    })
+}))
 
 document.getElementById('tecla_apagar').addEventListener('click', function(){
-    deleteValor(valor)
+    deleteValor(valor1)
 })
 
 
 document.getElementById('soma').addEventListener('click', function(){
-    document.getElementById('output_bar').innerText = valor.join("") + '+'
-    valor.push('+')
-    exibeNumeros('output_bar2')
+    document.getElementById('operat').innerText = '+'
+    num1 = parseInt(valor1.join(""))
+    console.log(num1)
+    deleteValor(valor1)
+    document.getElementById('result').innerText = num1
 })
 
 document.getElementById('resultado').addEventListener('click', function(){
     event.preventDefault()
-    valor1 = parseInt(document.getElementById('output_bar').innerText)
-    console.log(valor1)
-    valor2 = parseInt(document.getElementById('output_bar2').innerText)
-    console.log(valor2)
-    document.getElementById('output_bar').innerText = valor1 + valor2
+    num2 = parseInt(valor1.join(""))
+    console.log(num2)
+    result = num1 + num2
+    document.getElementById('result').innerText = `${num1} + ${num2} = ${result}`
 })
-
-/*document.getElementById('soma').addEventListener('click', function(){
-    document.getElementById('output_bar').innerText + '+'   
-})*/
