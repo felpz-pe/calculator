64a35be1432b5e15d84b4a9e7d705b03894add3f
Felpz <fp19430@gmail.com>
2024-05-22 12:08:47 -0300

    atlz 4

--- javascript.js
+++ javascript.js
@@ -1,5 +1,9 @@
-const teclado = [document.getElementById('tecla_1'),
-document.getElementById('tecla_2'), document.getElementById('tecla_3')
+const teclado = [document.querySelector('.tecla_1'),
+document.querySelector('.tecla_2'), document.querySelector('.tecla_3'),
+document.querySelector('.tecla_4'),  document.querySelector('.tecla_5'),
+document.querySelector('.tecla_6'),  document.querySelector('.tecla_7'),
+document.querySelector('.tecla_8'),  document.querySelector('.tecla_9'),
+document.querySelector('.tecla_0')
 ]
 var valor1 = []
 var num1
@@ -8,42 +12,73 @@ var result
 
 function deleteValor(array){
     let a = array.pop()
-    document.getElementById('output_bar').innerText = array.join("")
+    document.querySelector('.output_bar').innerText = array.join("")
 }
 
-document.getElementById('tecla_limpar').addEventListener('click', function(){
+document.querySelector('.tecla_limpar').addEventListener('click', function(){
     valor1 = []
-    document.getElementById('output_bar').innerText = valor1.join("")
+    document.querySelector('.output_bar').innerText = valor1.join("")
 })
 
 teclado.forEach(macaco => 
     macaco.addEventListener('click', function(){
         event.preventDefault()
         valor1.push(teclado.indexOf(macaco) + 1)
-        document.getElementById('output_bar').innerText = valor1.join("")
-        console.log(document.getElementById('output_bar').innerText)
+        document.querySelector('.output_bar').innerText = valor1.join("")
+        console.log(document.querySelector('.output_bar').innerText)
 }))
 
-document.getElementById('tecla_apagar').addEventListener('click', function(){
+document.querySelector('.tecla_apagar').addEventListener('click', function(){
     deleteValor(valor1)
 })
 
 
-document.getElementById('soma').addEventListener('click', function(){
-    document.getElementById('operat').innerText = '+'
+document.querySelector('.soma').addEventListener('click', function(){
+    document.querySelector('.operat').innerText = '+'
     num1 = parseInt(valor1.join(""))
     console.log(num1)
     valor1 = []
-    document.getElementById('output_bar').innerText = valor1.join("")
+    document.querySelector('.output_bar').innerText = valor1.join("")
     console.log(valor1[0])
-    document.getElementById('result').innerText = num1
+    document.querySelector('.result').innerText = num1
 })
 
-document.getElementById('resultado').addEventListener('click', function(){
+document.querySelector('.subtracao').addEventListener('click', function(){
+    document.querySelector('.operat').innerText = '-'
+    num1 = parseInt(valor1.join(""))
+    console.log(num1)
+    valor1 = []
+    document.querySelector('.output_bar').innerText = valor1.join("")
+    console.log(valor1[0])
+    document.querySelector('.result').innerText = num1
+})
+
+document.querySelector('.multip').addEventListener('click', function(){
+    document.querySelector('.operat').innerText = 'x'
+    num1 = parseInt(valor1.join(""))
+    console.log(num1)
+    valor1 = []
+    document.querySelector('.output_bar').innerText = valor1.join("")
+    console.log(valor1[0])
+    document.querySelector('.result').innerText = num1
+})
+
+
+document.querySelector('.resultado').addEventListener('click', function(){
     event.preventDefault()
-    document.getElementById('output_bar').innerText = ''
+    document.querySelector('.output_bar').innerText = ''
     num2 = parseInt(valor1.join(""))
     console.log(num2)
-    result = num1 + num2
-    document.getElementById('result').innerText = `${num1} + ${num2} = ${result}`
+    if (document.querySelector('.operat').innerText === '+'){
+        result = num1 + num2
+        document.querySelector('.result').innerText = `${num1} + ${num2} = ${result}`
+    } else if (document.querySelector('.operat').innerText === '-'){
+        result = num1 - num2
+        document.querySelector('.result').innerText = `${num1} - ${num2} = ${result}`
+    } else if (document.querySelector('.operat').innerText === 'x'){
+        result = num1 * num2
+        document.querySelector('.result').innerText = `${num1} x ${num2} = ${result}`
+    }
+    
+    
 })
